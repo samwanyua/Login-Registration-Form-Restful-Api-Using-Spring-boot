19156e3e08d0df6dd34bb7913d09b1293d45f0d6
samwanyua <samexample8@gmail.com>
2024-06-22 17:36:52 +0300

    Create html error page

--- Log in/Log-in/src/main/java/com/wanyua/Log/in/Controller/UserController.java
+++ Log in/Log-in/src/main/java/com/wanyua/Log/in/Controller/UserController.java
@@ -1,18 +1,36 @@
 package com.wanyua.Log.in.Controller;
 
+import com.wanyua.Log.in.Entity.User;
+import com.wanyua.Log.in.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/register")
-    public String getRegisterPage() {
+    public String getRegisterPage(Model model) {
+        model.addAttribute("registerRequest", new User());
         return "register";
     }
 
     @GetMapping("/login")
-    public String getLogIn(){
+    public String getLoginPage(Model model){
+        model.addAttribute("loginRequest", new User());
         return "login";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute User user){
+        System.out.println("Register request: " + user);
+        User registeredUser = userService.registerUser(user.getLogin(), user.getPassword(), user.getEmail());
+        return registeredUser == null ? "error_page" : "redirect/login";
     }
 
 }
