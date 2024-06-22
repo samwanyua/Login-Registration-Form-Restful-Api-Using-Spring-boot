3adef14a07b0059727747e0a71f9a68caaf00cdd
samwanyua <samexample8@gmail.com>
2024-06-22 12:25:38 +0300

    Implement user login logic

--- Log in/Log-in/src/main/java/com/wanyua/Log/in/Service/UserService.java
+++ Log in/Log-in/src/main/java/com/wanyua/Log/in/Service/UserService.java
@@ -1,16 +1,25 @@
 package com.wanyua.Log.in.Service;
 
 import com.wanyua.Log.in.Entity.User;
+import com.wanyua.Log.in.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
 
     public User registerUser(String login, String password, String email){
         if(login != null && password != null){
             User user = new User();
-            user.setLogin();
-
+            user.setLogin(login);
+            user.setPassword(password);
+            user.setEmail(email);
+            return userRepository.save(user);
+        }
+        else{
+            return null;
         }
     }
 }
