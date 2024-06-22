8fe77a5887cb3f208308502be9ee98c9d694939e
samwanyua <samexample8@gmail.com>
2024-06-22 11:24:42 +0300

    Initial commit

---
+++ Log in/Log-in/src/main/java/com/wanyua/Log/in/LogInApplication.java
@@ -0,0 +1,13 @@
+package com.wanyua.Log.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogInApplication.class, args);
+	}
+
+}
